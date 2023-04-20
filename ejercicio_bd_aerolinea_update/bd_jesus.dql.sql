9cdafb130c02fd0da49a6ce498fe799e46d26606
Lotito30 <dominguezje21@gmail.com>
2023-04-20 11:39:27 -0300

    uniendo dos commits 20_04 y 25_04

--- ejercicio_bd_aerolinea_update/bd_jesus.dql.sql
+++ ejercicio_bd_aerolinea_update/bd_jesus.dql.sql
@@ -7,6 +7,18 @@ select * from piloto_personal;
 select * from pilotos;
 select * from personas;
 
+
+-- vuelo minimo
+select ciudad,min(precio) from vuelos; 
+-- vuelo maximo
+select ciudad, max(precio) from vuelos; 
+-- consulta condicional
+select ciudad, precio from vuelos where precio >= 100000; 
+select ciudad, precio from vuelos where precio <= 200000; 
+-- dos ciudades
+select ciudad, precio from vuelos where ciudad in ("Caracas", "Nueva York");
+-- datos del pasajero
+select distinct * from personas join pasajeros on pasajeros.pasaporte = personas.pasaporte;
 -- el precio minimo
 select min(precio) from vuelos;
 -- el precio maximo
@@ -21,9 +33,20 @@ select horaSalida, horaLlegada from vuelos;
 select horaLlegada, horaSalida, format((horaLlegada - horaSalida) * 0.01, 2) as horasTotales from vuelos;
 -- cuantos viajan hasta venezuela
 select count(pasaporte), ciudad from pasajeros join vuelos on pasajeros.nro_vuelo = vuelos.nro
-where ciudad = "caracas";
+where ciudad = "Caracas";
 -- average
-select format(avg(precio),2) as promedio from vuelos where ciudad = "caracas";
+select format(avg(precio),2) as promedio from vuelos where ciudad = "Caracas";
+ 
+select count(pasaporte) as cantidad, ciudad from pasajeros join vuelos on pasajeros.nro_vuelo = vuelos.nro
+where ciudad = "Caracas";
+-- calcular descuentos 20%
+select ciudad, precio, format((precio * 0.80),2) descuento20  from vuelos;
+-- descuento mas barato
+select ciudad, precio, min(precio*0.8) as descuento20 from vuelos;
+-- calcular average
+select count(ciudad) as cantidad, ciudad, format(avg(precio),2) as average from vuelos where ciudad="Caracas";
+-- group by
+select count(ciudad) as cantidad,ciudad, sum(precio), format((precio * 0.80),2) as descuento20  from vuelos group by ciudad ;
 -- crear variable
 set @minPrecio = (select min(precio) from vuelos);
 set @maxPrecio = (select max(precio) from vuelos);
@@ -50,8 +73,9 @@ left join pasajeros p on v.nro = p.nro_vuelo;
  
  -- precio total de una ciudad
  select sum(precio), ciudad from vuelos 
- where ciudad = "caracas";
+ where ciudad = "Caracas";
  
  -- Sirve para hacer funciones max(),min(),avg(). Busca los campos que estan dentro de la condicion 
  select sum(precio), ciudad from vuelos
- group by ciudad having sum(precio) < (select avg(precio) from vuelos where ciudad="santiago");
+ group by ciudad having sum(precio) < (select avg(precio) from vuelos where ciudad="Caracas");
+
