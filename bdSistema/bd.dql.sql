849e3eee3556de8e010d31c47b4669a4bd94326a
Lotito30 <dominguezje21@gmail.com>
2023-05-09 09:37:43 -0300

    completar consultas en el dql

--- bdSistema/bd.dql.sql
+++ bdSistema/bd.dql.sql
@@ -11,14 +11,19 @@ join sistemas s on s.nivel = sp.nivel_sistema
 join programas p on p.freq_ejec = sp.freq_ejec
 where s.nivel = "bajo" and p.freq_ejec = "media";
 
+-- traer la suma de veces que se ejecutaron programas en el sistem
+select count(id) as freq_ejec_programa from sistemas_programas
+join  sistemas on sistemas.nivel= sistemas_programas.nivel_sistema;
+
 -- traer la suma de veces que se ejecutaron programas en el sistema medio
-select sp.id,s.nivel from sistemas_programas sp
+select count(sp.id) as Total,p.freq_ejec,s.nivel from sistemas_programas sp
 join sistemas s on s.nivel = sp.nivel_sistema
 join programas p on p.freq_ejec = sp.freq_ejec
 where s.nivel = "medio";
 
 -- traeme los programas con archivos Input-Output
-select pa.id, a.modos from programas_archivos pa
+select pa.id, pa.freq_ejec, a.modos from programas_archivos pa
+join programas p on p.freq_ejec = pa.freq_ejec
 join archivos a on a.modos = pa.modos
 where a.modos = "Input-Output";
 
